e8d7d6e1f0c8cc1bc22cf6e0113e2f92a97a885c
Ahmad Obiedat <alakmo8@gmail.com>
2023-08-03 22:49:34 -0700

    Time: 70 ms (31.45%), Space: 17.1 MB (58.50%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) -1
+
+        while l < r:
+            if not self.al(s[l]):
+                l += 1
+                continue
+            if not self.al(s[r]):
+                r -= 1
+                continue
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
+    def al(self, c):
+        return (
+            ord("A") <= ord(c) <= ord("Z") or
+            ord("a") <= ord(c) <= ord("z") or
+            ord("0") <= ord(c) <= ord("9")
+        )
+            
