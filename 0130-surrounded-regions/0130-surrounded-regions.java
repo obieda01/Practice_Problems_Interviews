e27c9b122c591cd803a22f838daca975412623d0
Ahmad Obiedat <alakmo8@gmail.com>
2024-04-09 15:04:09 -0400

    Time: 1 ms (99.56%), Space: 45.8 MB (16.14%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,34 +1,40 @@
 class Solution {
-    public void solve(char[][] board) {
-        int r = board.length;
-        int c = board[0].length;
 
-        for(int i = 0; i < c; i++){
-            if(board[0][i] == 'O') dfs(board, 0, i);
-            if(board[r-1][i] == 'O') dfs(board, r -1, i);
+    public void solve(char[][] board) {
+        int nRows = board.length;
+        int nCols = board[0].length;
+
+        // 1a) Capture unsurrounded regions - top and bottom row (O -> T)
+        for (int i = 0; i < nCols; i++) {
+            if (board[0][i] == 'O') dfs(board, 0, i);
+            if (board[nRows - 1][i] == 'O') dfs(board, nRows - 1, i);
         }
-        for(int i = 0; i < r; i++){
-            if(board[i][0] == 'O') dfs(board, i, 0);
-            if(board[i][c-1] == 'O') dfs(board, i, c-1);
+
+        // 1b) Capture unsurrounded regions - Left and right columns (O -> T)
+        for (int i = 0; i < nRows; i++) {
+            if (board[i][0] == 'O') dfs(board, i, 0);
+            if (board[i][nCols - 1] == 'O') dfs(board, i, nCols - 1);
         }
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (board[i][j] == 'O') board[i][j] = 'X'; // 2) Capture surrounded regions (O -> X)
-                if (board[i][j] == 'T') board[i][j] = 'O'; // 3) Uncapture unsurrounded regions (T- O)
+
+        for (int r = 0; r < nRows; r++) {
+            for (int c = 0; c < nCols; c++) {
+                if (board[r][c] == 'O') board[r][c] = 'X'; // 2) Capture surrounded regions (O -> X)
+                if (board[r][c] == 'T') board[r][c] = 'O'; // 3) Uncapture unsurrounded regions (T- O)
             }
         }
-
     }
-        private void dfs(char[][] board, int r, int c) {
+
+    private void dfs(char[][] board, int r, int c) {
         int nRows = board.length;
         int nCols = board[0].length;
         if (
             r < 0 || c < 0 || r >= nRows || c >= nCols || board[r][c] != 'O'
         ) return;
+
         board[r][c] = 'T';
         dfs(board, r + 1, c);
         dfs(board, r - 1, c);
         dfs(board, r, c + 1);
-        dfs(board, r, c - 1);         
-        }
+        dfs(board, r, c - 1);
+    }
 }
