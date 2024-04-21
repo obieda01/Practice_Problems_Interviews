0d8d85603c313f5c6591536dafed635af6aeb74e
Ahmad Obiedat <alakmo8@gmail.com>
2024-04-21 16:25:40 -0400

    Time: 12 ms (95.27%), Space: 53.2 MB (44.00%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,23 +1,27 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
+        //calculate all distances 
         int[] distances = new int[points.length];
         for(int i = 0; i < points.length; i++){
-            distances[i] = calDis(points[i]);
+            distances[i] = calDist(points[i]);
         }
-        //[1, 5, 3]
+        //sort calculated distance
         Arrays.sort(distances);
-        //[1, 3, 5]
-        int distK = distances[k - 1];
-        int[][] distancesK = new int[k][2];
+        //find the k - 1 distance to use as threshold
+        int distancek = distances[k - 1];
+        //keep track ans index
         int index = 0;
+        //add to ans if less than k-1 distance
+        int[][] ans = new int[k][2];
         for(int i = 0; i < points.length; i++){
-            if(calDis(points[i]) <= distK){
-                distancesK[index++] = points[i];
+            if(calDist(points[i]) <= distancek){
+                ans[index] = points[i];
+                index++;
             }
         }
-        return distancesK;
+        return ans;
     }
-    public int calDis(int[] point){
+    public int calDist(int[] point){
         return point[0]*point[0] + point[1]*point[1];
     }
 }
