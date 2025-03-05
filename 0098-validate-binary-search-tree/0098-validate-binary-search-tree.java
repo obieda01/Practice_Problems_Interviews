b5e540a18cde05c36a3dde0364db2aaffba5f76a
Ahmad Obiedat <alakmo8@gmail.com>
2025-03-05 00:41:23 -0600

    Time: 0 ms (100%), Space: 43.5 MB (60.63%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,30 +1,13 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return dfs(root, null, null);
+        return valid(root, Long.MIN_VALUE, Long.MAX_VALUE);        
     }
-    public boolean dfs(TreeNode node, Integer min, Integer max){
-        if(node == null) return true;
-        if((min != null && min >= node.val)
-         || (max != null && max <= node.val))
-         {
-            return false;
-        }
-        return dfs(node.left, min, node.val) && dfs(node.right, node.val, max);
-    }
-    
+
+    private boolean valid(TreeNode node, long minimum, long maximum) {
+        if (node == null) return true;
+
+        if (!(node.val > minimum && node.val < maximum)) return false;
+
+        return valid(node.left, minimum, node.val) && valid(node.right, node.val, maximum);
+    }    
 }
