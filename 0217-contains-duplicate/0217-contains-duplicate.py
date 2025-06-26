89555ec6baf0420769e198868187bd5a82bd8f3f
Ahmad Obiedat <alakmo8@gmail.com>
2025-06-26 21:23:38 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,8 +1,3 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        cache = set()
-        for num in nums:
-            if num in cache:
-                return True
-            cache.add(num)
-        return False  
+        
